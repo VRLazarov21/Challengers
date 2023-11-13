1b9166703f42606c6bc154b0cb9a3efe567a36f5
Alexsander Sivkov <ATSivkov21@codingburgas.bg>
2023-11-13 22:15:35 +0200

    Update Challengers file

--- Challengers/Challengers/Challengers/Challengers.cpp
+++ Challengers/Challengers/Challengers/Challengers.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
-#include <Find.h>
-#include <Menu.h>
-#include <Table.h>
+#include "Find.h"
+#include "Menu.h"
+#include "Table.h"
 using namespace std;
 
 int main() {
 	menu();
-	find();
 	table();
+	find();
+	TextElement();
 }
